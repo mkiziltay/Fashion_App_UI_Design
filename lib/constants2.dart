16f2ecdb9f1591cc68ba716e50da1a91004bbb76
Mustafa KIZILTAY <mkiziltay@hotmail.com>
2022-03-09 00:56:05 +0300

    basic

--- lib/constants2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Colors that we use in our app
-const kPrimaryColora = Color(0xFF0C9869);
-const kTextColora = Color(0xFF3C4046);
-const kBackgroundColora = Color(0xFFF9F8FD);
-
-const double kDefaultPaddinga = 20.0;
